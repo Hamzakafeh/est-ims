bac1a4c1a51308372f68ca8cd52cf28a98d0be1c
Mlo5 <hamzakafeh@gmail.com>
2026-05-11 20:36:16 +0300

    update

--- iMs.bat
+++ iMs.bat
@@ -31,17 +31,12 @@
 ::
 ::
 ::978f952a14a936cc963da21a135fa983
+
 @echo off
-for /f "delims=" %%a in ('powershell -Command "$p = Read-Host ''What Do You Want?'' -AsSecureString; [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($p))"') do set pass=%%a
-if NOT "%pass%"=="192.168.100.1" (
-    echo Wrong Password!
-    timeout /t 2 >nul
-    exit
-)
-echo Welcome Mlo5
-timeout /t 1 >nul
+
 title EST Inventory System - Alestesharia Animal Nutrition
 color 0B
+
 echo.
 echo  ================================================
 echo   EST Inventory Management System®
@@ -49,10 +44,16 @@ echo   Alestesharia Animal Nutrition®
 echo   Created By Hamza K. Ghareb™
 echo  ================================================
 echo.
+
+REM Environment Variables
+set APP_USERNAME=Mlo5
+set APP_PASSWORD=Lara3049
+set SECRET_KEY=LARALOLO
+
 echo  [*] Starting server...
-echo  [*] The browser will open automatically
-echo  [*] Press Ctrl+C to stop the server
+echo  [*] Browser will open automatically...
 echo.
+
 REM Check Python
 python --version >nul 2>&1
 if errorlevel 1 (
@@ -60,18 +61,25 @@ if errorlevel 1 (
     pause
     exit /b 1
 )
+
 REM Check Flask
 python -c "import flask" >nul 2>&1
 if errorlevel 1 (
     echo  [*] Installing Flask...
-    py -m pip install flask openpyxl flaskwebgui
+    py -m pip install flask openpyxl gunicorn
 )
+
 REM Check openpyxl
 python -c "import openpyxl" >nul 2>&1
 if errorlevel 1 (
     echo  [*] Installing openpyxl...
     py -m pip install openpyxl
 )
-REM Run app
+
 cd /d "%~dp0"
+
+start http://127.0.0.1:3049
+
 python app.py
+
+pause
